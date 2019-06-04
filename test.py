f8cc007df3351660cfd424c57345a6041b959c27
alexrecuenco <safale93@gmail.com>
2019-06-04 02:43:12 +0200

    Fixed N_POINTS being fixed by mistake

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from kmeans import KMeans
 
 from random import random
 
-N_CLUSTER = 10
-N_POINTS = 100
-N_DIMENS = 2
+N_CLUSTER = 3
+N_POINTS = 1000
+N_DIMENS = 5
 
 
 def rand_point(n_dimens):
@@ -15,7 +15,7 @@ def rand_point(n_dimens):
 def main():
     # Actually testing if the implementation works, would require to test agains ta current model...
     # Here we are just asserting that it doesn't do anything crazy, and that it terminates
-    points = list(rand_point(N_DIMENS) for _ in range(100))
+    points = list(rand_point(N_DIMENS) for _ in range(N_POINTS))
     kmeans = KMeans(N_CLUSTER).fit(points)
 
     predictions = kmeans.predict(points)
